a58e60c8e1cd00997644817f568d684a566d5c80
xjt <969211735@qq.com>
2024-10-25 11:39:34 +0800

    veri EOF

--- inc/sgjw.c
+++ inc/sgjw.c
@@ -11,16 +11,14 @@
  * @retval -2, malloc buffer fail.
  * @retval -3, read from file fail.
  */
-static int8_t Open_File_In_Binary(const char* filepath, uint8_t** buffer)
+static size_t Open_File_In_Binary(const char* filepath, uint8_t** buffer)
 {
-    int8_t retval = 0;
-
     /* ----- Step 1 : Open File ----- */
 
     FILE* file = fopen(filepath, "rb");
     if (file == NULL)
     {
-        retval = -1;
+        printf("No such file: [%s].\n", filepath);
         goto direct_return;
     }
 
@@ -35,7 +33,7 @@ static int8_t Open_File_In_Binary(const char* filepath, uint8_t** buffer)
     *buffer = (uint8_t*)malloc(file_size * sizeof(uint8_t));
     if (*buffer == NULL)
     {
-        retval = -2;
+        printf("Malloc buffer fail.\n");
         goto close_return;
     }
 
@@ -44,35 +42,75 @@ static int8_t Open_File_In_Binary(const char* filepath, uint8_t** buffer)
     size_t read_size = fread(*buffer, 1, file_size, file);
     if (read_size != file_size)
     {
-        free(*buffer);
-        retval = -3;
+        printf("Buffer size not equal file size.\n");
+        goto free_return;
     }
 
+    return read_size;
+
+free_return:
+    free(*buffer);
 close_return:
     fclose(file);
 direct_return:
+    return 0;
+}
+
+static int8_t File_Verification_EOF(uint8_t* buffer, size_t buffer_size)
+{
+    int8_t retval = 0;
+    const uint8_t expected_end[] = {0x37, 0x66, 0x07, 0x1A, 0x12, 0x3A, 0x4C, 0x9F, 0xA9, 0x5D, 0x21, 0xD2, 0xDA, 0x7D, 0x26, 0xBC};
+    const size_t expected_end_size = sizeof(expected_end);
+
+    if (buffer_size < expected_end_size)
+    {
+        retval = -1;
+    }
+    else
+    {
+        size_t offset = buffer_size - expected_end_size;
+        if (memcmp(buffer + offset, expected_end, expected_end_size) != 0)
+        {
+            retval = -1;
+        }
+    }
+
     return retval;
 }
 
-int8_t State_Grid_JPEG_Reader(const char* path)
+    // 2. get offsite:
+    // 4 bytes before [0x37 0x66 0x07 0x1a 0x12 0x3a 0x4c 0x9f 0xa9 0x5d 0x21 0xd2 0xda 0x7d 0x26 0xbc]
+
+int8_t State_Grid_JPEG_Reader(const char* filepath)
 {
     int8_t retval = 0;
+    int8_t ret;
 
     /* ----- Step 1 : Read Data ----- */
 
     uint8_t* _bin_original = NULL;
-    int8_t ret_original = Open_File_In_Binary(path, &_bin_original);
-    if (ret_original != 0)
+    size_t buffer_size = Open_File_In_Binary(filepath, &_bin_original);
+    if (buffer_size == 0)
     {
         retval = -1;
-        printf("Read file [%s] failed with error code: %d\n", path, ret_original);
+        printf("Read file: [%s] failed.\n", filepath);
         goto free_return;
     }
-
     printf("Read Success\n");
 
+    /* ----- Step 2 : Verification ----- */
+
+    ret = File_Verification_EOF(_bin_original, buffer_size);
+    if (ret != 0)
+    {
+        retval = -2;
+        printf("File EOF label verification fail.\n");
+        goto free_return;
+    }
+    printf("Verification Success\n");
+
 free_return:
-    if (ret_original == 0)
+    if (buffer_size > 0)
         free(_bin_original);
 
     return retval;
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "../inc/sgjw.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    State_Grid_JPEG_Reader("1.jpg");
+    State_Grid_JPEG_Reader(argv[1]);
     return 0;
 }
