7eee8ea7962dd739f5b49f528e12ab36649f86e3
xjt <969211735@qq.com>
2024-10-29 10:56:49 +0800

    Feat: version 0.1
    1. read metadata from jpeg file, with auto allocate mem;
    2. write metadata to file;
    3. release mem.

--- inc/sgjw.h
+++ inc/sgjw.h
@@ -1,5 +1,28 @@
 #pragma once
 
+/**
+ * @file sgjw.h
+ * @author master@xiaojintao.email
+ * @brief State Grid JPEG Wrapper (SGJW) - A C Library for Embedding and Extracting Metadata in JPEG Files
+ * @version 0.1
+ * @date 2024-10-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ * @note Typical usage for reading:
+ * 1. Instantiate an object of the @struct StateGridJPEG, named _obj.
+ * 2. Invoke the State_Grid_JPEG_Read(filepath, &_obj) function to read the JPEG file specified by filepath and populate _obj with the extracted metadata.
+ * 3. Access the desired metadata fields through the _obj members.
+ * 4. Call the State_Grid_JPEG_Delete_OBJ function to deallocate the memory associated with _obj.
+ * 
+ * @note Typical usage for writing:
+ * 1. Instantiate an object of the @struct StateGridJPEG, named _obj.
+ * 2. Populate all the members of _obj with the desired metadata values.
+ * 3. Utilize a JPEG library (e.g. libjpeg) or other suitable libraries to save a JPEG file, named _file.
+ * 4. Invoke the State_Grid_JPEG_Append(_file, &_obj) function to append the metadata stored in _obj to the end of _file using steganography techniques.
+ * 5. Call the State_Grid_JPEG_Delete_OBJ function to deallocate the memory associated with _obj.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -23,7 +46,7 @@ typedef enum
     SGJW_ERROR_INVALID_PARAMS = -7,
     SGJW_ERROR_MEMORY_ALLOCATION = -8,
     SGJW_ERROR_FILE_WRITE = -9,
-    SGJW_ERROR_FIELD_SET_FAILED = 10
+    SGJW_ERROR_FIELD_SET_FAILED = -10
 } SGJW_ERROR;
 
 // Main structure
@@ -70,27 +93,28 @@ typedef struct
 } StateGridJPEG;
 
 /**
- * @brief Read JPEG file and parse its content.
+ * @brief Read a JPEG file and parse its embedded metadata.
  * 
- * @param filepath Path to the JPEG file.
- * @param obj Pointer to StateGridJPEG structure.
- * @return SGJW_ERROR code.
+ * @param filepath The path to the JPEG file to be read.
+ * @param obj A pointer to the StateGridJPEG structure that will store the parsed metadata.
+ * @return An SGJW_ERROR code indicating the success or failure of the operation.
  */
 int8_t State_Grid_JPEG_Read(const char* filepath, StateGridJPEG* obj);
 
 /**
- * @brief Append SGJW information to jpeg file.
+ * @brief Append SGJW metadata to a JPEG file.
  * 
- * @param filepath Path to output file.
- * @param obj Pointer to StateGridJPEG structure.
- * @return SGJW_ERROR code.
+ * @param filepath The path to the output JPEG file.
+ * @param obj A pointer to the StateGridJPEG structure containing the metadata to be appended.
+ * @return An SGJW_ERROR code indicating the success or failure of the operation.
  */
+
 int8_t State_Grid_JPEG_Append(const char* filepath, StateGridJPEG* obj);
 
 /**
- * @brief Free all allocated memory in StateGridJPEG structure.
+ * @brief Deallocate the memory associated with a StateGridJPEG structure.
  * 
- * @param obj Pointer to StateGridJPEG structure.
+ * @param obj A pointer to the StateGridJPEG structure to be deallocated.
  */
 void State_Grid_JPEG_Delete_OBJ(StateGridJPEG* obj);
 
